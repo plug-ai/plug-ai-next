db711249f308a25aa0e1c8616b3de760b7c67551
piotr.tomasiewicz <piotr.tomasiewicz@ardentcode.com>
2024-02-16 15:54:05 +0100

    wip

--- ai-plugin/api-docs.ts
+++ ai-plugin/api-docs.ts
@@ -2,13 +2,39 @@ import { createSwaggerSpec } from 'next-swagger-doc';
 
 export const getApiDocs = async () => {
     const spec = createSwaggerSpec({
-        apiFolder: 'src/app/api',
+        apiFolder: 'app/api',
         definition: {
             openapi: '3.0.0',
             info: {
                 title: 'Next Swagger API',
                 version: '1.0',
             },
+            servers: [
+                {
+                    url: process.env.BASE_URL || 'http://localhost:3000',
+                    description: 'Test',
+                },
+            ],
+            components: {
+                securitySchemes: {
+                    "OAuth2": {
+                        type: 'oauth2',
+                        flows: {
+                            authorizationCode: {
+                                authorizationUrl: 'https://plugai.loca.lt/api/plugin/authorize',
+                                tokenUrl: 'https://plugai.loca.lt/api/plugin/oauth/token',
+                                scopes: {}
+                            }
+                        }
+                    }
+                },
+                schemas: {}
+            },
+            security: [
+                {
+                    "OAuth2": []
+                }
+            ]
         },
     });
 
